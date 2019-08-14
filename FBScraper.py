f4ed6253feb093508a576a10237ba3cbc2e9ac30
Nihinlolamiwa Fajemilehin <nihinfaj@gmail.com>
2019-08-14 13:17:38 +0100

    input file read

--- FBScraper.py
+++ FBScraper.py
@@ -3,16 +3,48 @@
 
 import json
 import facebook
+# The library needed to read an excel file using Python 
+from openpyxl import Workbook
+from openpyxl import load_workbook
 
-# Function that reads the Access Token and Page ID from a 
-def funcname(self, parameter_list):
-    pass
+# The location of the input file 
+filename_root = 'C:/Users/Synergos/OneDrive/Documents/SusMon_Facebook_API'
+input_filename = filename_root + '/FBPage_Input.xlsx'
+
+# Function that reads Access Token and Page ID from an Excel file
+def getInputData():
+    
+    print("I am about to attempt opening this document")
+    #open our excel input workbook
+    wb_input = load_workbook(filename=input_filename,read_only=True)
+    
+    # select FBPage_Input.xlsx
+    sheet = wb_input.active
+
+    # get Page ID
+    pageIdLabel = sheet['B1']
+    pageIdValue = sheet['B2']
+    pageIdValue = round(pageIdValue.value)
+
+    # get Brand name
+    pageNameLabel = sheet['C1']
+    pageNameValue = sheet['C2']
+    pageNameValue = pageNameValue.value
+
+    # get Access Token
+    pageAccessTokenLabel = sheet['D1']
+    pageAccessTokenValue = sheet['D2']
+    pageAccessTokenValue = pageAccessTokenValue.value
+    
 
 def main():
-    token = "EAARN1Aj3c2wBAIIzqVuqB0GuYMJcXzcg608oYfZCwLaYoXOGMkI3PeWM7Fprd0c2O8sWBZB7H4ZA96n2dGSuYM8fg7uuROsZAAZBU2lIXxdH8ahVuIb0fgjaWb52ZAcfTlp2BZAPMH2yfXc4EN2BfAr1PGyrgZBFIgedrFJQ4A9jSldSdtUYFlZCJkHbHOCOaC9oZD"
+    # Call function to get input data
+    getInputData()
+
+    token = pageAccessTokenValue
     graph = facebook.GraphAPI(token)
     #fields = ['first_name', 'location{location}','email','link']
-    profile = graph.get_object('107513663924342',fields='name,fan_count,posts,link')
+    profile = graph.get_object(pageIdValue,fields='name,fan_count,posts,link')
     #return desired fields
     print(json.dumps(profile, indent=4))
 
